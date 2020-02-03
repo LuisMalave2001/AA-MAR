a910249890ca417679c4a9b3db6021e2db1ebd70
LuisMora <LuisMora@DESKTOP-90891QA>
2020-02-03 12:15:46 +0100

    Campo name requerido en languages

--- adm_uni/models/languages.py
+++ adm_uni/models/languages.py
@@ -6,10 +6,10 @@ from odoo import models, fields
 class AdmissionLanguages(models.Model):
     _name = "adm_uni.languages"
     
-    name = fields.Char("Name")
+    name = fields.Char("Name", required=True)
     
 class AdmissionLanguageLevels(models.Model):
     _name = "adm_uni.languages.level"
     
-    name = fields.Char("Name")
+    name = fields.Char("Name", required=True)
     
