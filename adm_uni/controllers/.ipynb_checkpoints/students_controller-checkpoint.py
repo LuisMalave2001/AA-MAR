f888fd396676383ccf83a9fec94f7d2ced0bf4c4
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-10 09:05:23 +0000

    [ADD] Añadido controller get students

---
+++ adm_uni/controllers/.ipynb_checkpoints/students_controller-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from . import base_controller as base
+import json
+
+
+class StudentController(http.Controller):
+    @http.route("/admission/adm_uni", auth="public", methods=["GET"])
+    def get_adm_uni(self, **params):
+        # print(http.request.httprequest.args.getlist("test"))
+        students = http.request.env['adm_uni.inquiry']
+        search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
+        students_record = students.search(search_domain)
+        students_values = states_record.read(["first_name", "last_name","name","id"])
+        # states.sudo().write();
+        return json.dumps(students_values)
