eac7f8df55277c91b30389bb4cf621961b857be2
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-10 11:39:57 +0000

    Reparar errores

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -16,9 +16,9 @@ class StudentController(http.Controller):
         
         students_values = students_record.read(["id","birthdate","city","country_id","create_date","create_uid","current_school","current_school_address","email","first_name","gender","last_name","name","phone","state_id","street_address","write_date","write_uid","zip"])
         
-        student_values["birthdate"] = str(student_values["birthdate"])
-        student_values["create_date"] = str(student_values["create_date"])
-        student_values["write_date"] = str(student_values["write_date"])
+        students_values["birthdate"] = str(student_values["birthdate"])
+        students_values["create_date"] = str(student_values["create_date"])
+        students_values["write_date"] = str(student_values["write_date"])
         
         # states.sudo().write();
         return json.dumps(students_values)
