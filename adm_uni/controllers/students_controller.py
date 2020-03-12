9802a8401ba09504fe992ae9aaf15f376f62c962
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-12 07:19:57 +0000

    Prueba codigo

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -9,8 +9,8 @@ class StudentController(http.Controller):
     def get_adm_uni(self, **params):
         # print(http.request.httprequest.args.getlist("test"))
         students = http.request.env['adm_uni.inquiry']
-        # search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
-        # students_record = students.search(search_domain)
+        search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
+        students_record = students.search(search_domain)
         # students_values = students_record.read(["id","city","first_name"]) 
         students_values = students_record.read(["id","first_name"]) 
         return json.dumps(states_values) 
