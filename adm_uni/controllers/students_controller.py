2f1c3fb6788580c726184324aa678d685eb9e22f
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-13 07:41:18 +0000

    Modificar id-country

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -9,7 +9,8 @@ class StudentController(http.Controller):
     def get_adm_uni(self, **params):
         # print(http.request.httprequest.args.getlist("test"))
         students = http.request.env['adm_uni.inquiry']
-        search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
+        search_domain = [(int(params['country_id']))] if "country_id" in params else []
+        # search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
         students_record = students.search(search_domain)
         students_values = students_record.read(["id","city","first_name","country_id"]) 
         return json.dumps(students_values)
