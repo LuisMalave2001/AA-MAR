346e50352099386bdca213d8d169f7a19d1b3708
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-16 11:29:03 +0000

    Fixes birthdate

--- adm_uni/controllers/.ipynb_checkpoints/students_controller-checkpoint.py
+++ adm_uni/controllers/.ipynb_checkpoints/students_controller-checkpoint.py
@@ -3,6 +3,8 @@
 from odoo import http
 from . import base_controller as base
 import json
+from datetime import datetime
+from datetime import date
 
 class StudentController(http.Controller):
     @http.route("/admission/adm_uni", auth="public", methods=["GET"], cors='*')
@@ -10,8 +12,26 @@ class StudentController(http.Controller):
         # print(http.request.httprequest.args.getlist("test"))
         students = http.request.env['adm_uni.inquiry']        
         search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []  
-        students_record = students.search(search_domain)
-        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","last_name","name","email"])
-      
+        students_record = students.search(search_domain)        
+        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","last_name","name","email","birthdate"])
+        
+        date_of_birth = students_values["birthdate"]
+        date_of_birth = date_of_birth.strftime('%dd/%mm/%YYYY')
+        students_values["birthdate"] = date_of_birth
+        
+        # datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        # students_values_birthdate = "2020-03-03"
+
+#fecha_str = students_re[10]
+
+#date_object = datetime.strptime(fecha_str, '%dd/%mm/%Y')
+
+#fecha_str = datetime.strftime(date_object, '%dd/%mm/%Y')
+
+#students_values[9]= fecha_str     
+        
+
+        
         return json.dumps(students_values)
 
--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -13,7 +13,11 @@ class StudentController(http.Controller):
         students = http.request.env['adm_uni.inquiry']        
         search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []  
         students_record = students.search(search_domain)        
-        students_values = students_record.read([str("id"),"city","country_id","state_id", "street_address","zip","first_name","last_name","name","email",datetime.date.strftime("birthdate", '%dd/%mm/%YYYY')])
+        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","last_name","name","email","birthdate"])
+        
+        date_of_birth = students_values["birthdate"]
+        date_of_birth = date_of_birth.strftime('%dd/%mm/%YYYY')
+        students_values["birthdate"] = date_of_birth
         
         # datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
