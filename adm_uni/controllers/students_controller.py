c422d85a639fb120387d5ef40f925fc7a8ac9fdd
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-19 10:17:41 +0000

    test add array

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -6,29 +6,37 @@ import json
 from datetime import datetime
 from datetime import date
 
-
+#controlador encargado de devolver data de estudiantes, para insertarlo en FACTS
 class StudentController(http.Controller):
+    #definiendo la url desde donde va ser posible acceder, tipo de metodo, cors para habiltiar accesos a ip externas.
     @http.route("/admission/adm_uni", auth="public", methods=["GET"], cors='*')
-    def get_adm_uni(self, **params):
-        # print(http.request.httprequest.args.getlist("test"))
+    # define una funcion principal
+    def get_adm_uni(self, **params): 
+        #crea una variable con el modelo desde donde se va a tomar la información
         students = http.request.env['adm_uni.inquiry']        
-        search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []  
-        students_record = students.search(search_domain)        
+        
+        #filtro del modelo basados en parametros de la url
+        search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []
+        
+        #Tomar informacion basado en el modelo y en el domain IDS
+        students_record = students.search(search_domain)      
+        
+        #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
         students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","middle_name","last_name","name","email","birthdate","gender","phone", "status_id","current_school","current_school_address","__last_update","create_date","create_uid","write_date","write_uid"])
 
         # Se recorre por cada estudiante
-        for record in students_values:
-            
+        for record in students_values: 
             # Convertir fechas a string
             record["birthdate"] = record["birthdate"].strftime('%m/%d/%Y')
             # Es lo mismo que:
             # date_of_birth = record["birthdate"]
             # date_of_birth = date_of_birth.strftime('%m/%d/%Y')
-            # record["birthdate"] = date_of_birth
-            
+            # record["birthdate"] = date_of_birth 
             record["__last_update"] = record["__last_update"].strftime('%m/%d/%Y')
             record["create_date"] = record["create_date"].strftime('%m/%d/%Y')
-            record["write_date"] = record["write_date"].strftime('%m/%d/%Y')          
-                    
+            record["write_date"] = record["write_date"].strftime('%m/%d/%Y')   
+                
+        students_values.append("test")
+        #pintar la información obtenida, esto lo utilizamos para parsearlo en el ajax.         
         return json.dumps(students_values)
 
