c09b516935962885e9b42a0f1b61db0325f70a4d
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-19 07:22:06 +0000

    Todos

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -14,30 +14,21 @@ class StudentController(http.Controller):
         students = http.request.env['adm_uni.inquiry']        
         search_domain = [("country_id", "=", int(params['country_id']))] if "country_id" in params else []  
         students_record = students.search(search_domain)        
-        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","middle_name","last_name","name","email","birthdate","gender","phone", "status_id","current_school","current_school_address","__last_update"])
+        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","middle_name","last_name","name","email","birthdate","gender","phone", "status_id","current_school","current_school_address","__last_update","create_date","create_uid","write_date","write_uid"])
 
-        
-        # , create_date, create_uid, write_date, write_uid
-
-
-        
-        
         # Se recorre por cada estudiante
         for record in students_values:
             
             # Convertir fechas a string
-            record["__last_update"] = record["__last_update"].strftime('%m/%d/%Y')
-            
-            
-            
             record["birthdate"] = record["birthdate"].strftime('%m/%d/%Y')
-            
             # Es lo mismo que:
             # date_of_birth = record["birthdate"]
             # date_of_birth = date_of_birth.strftime('%m/%d/%Y')
             # record["birthdate"] = date_of_birth
-
-
-        
+            
+            record["__last_update"] = record["__last_update"].strftime('%m/%d/%Y')
+            record["create_date"] = record["create_date"].strftime('%m/%d/%Y')
+            record["write_date"] = record["write_date"].strftime('%m/%d/%Y')          
+                    
         return json.dumps(students_values)
 
