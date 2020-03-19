e5f8cf22a6ff86a1230b8da2bb1d2188de441a98
LuisMalave2001 <58850261+LuisMalave2001@users.noreply.github.com>
2020-03-19 15:12:04 +0100

    Fixed

--- adm_uni/controllers/__init__.py
+++ adm_uni/controllers/__init__.py
@@ -3,7 +3,4 @@
 from . import admission_inquiry_controller
 from . import admission_application_controller
 from . import states_controller
-<<<<<<< Updated upstream
-=======
 from . import portal_controller
->>>>>>> Stashed changes
