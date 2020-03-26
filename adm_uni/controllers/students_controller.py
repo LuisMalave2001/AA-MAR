2f6b1616d2785ce6cc12a96975e7cd66883feb89
dconde@eduwebgroup.com <dconde@eduwebgroup.com>
2020-03-26 10:51:19 +0000

    test status_type

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -18,8 +18,8 @@ class StudentController(http.Controller):
         students = http.request.env['adm_uni.application']        
         
         #filtro del modelo basados en parametros de la url
-        search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))] if "country_id" in params else []
-        #search_domain = [("country_id", "=", int(params['country_id']))] 
+        #search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))] if "country_id" in params else []
+        search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))] 
         
         #Tomar informacion basado en el modelo y en el domain IDS
         students_record = students.search(search_domain)      
