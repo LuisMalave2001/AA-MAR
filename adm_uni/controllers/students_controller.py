4cfcd4bbc8e58686f9abe5cb9a357a892c093763
LuisMalave2001 <odoo@eupp13.odoo.com>
2020-03-31 07:24:00 +0000

    function update facts id v1

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -99,4 +99,4 @@ class StudentController(http.Controller):
             #actualizamos campo
             obj.sudo().write({'x_facts_id': itemData["factsId"]}) 
         
-        return json.dumps(application_values[0]["id"])
+        return json.dumps('correcto')
