6e6abd526653cf927f6d4a421ff9ae4ac82dd0c6
DavidCondeCubas <dconde@eduwebgroup.com>
2020-03-31 05:17:03 +0000

    add crsf=false

--- adm_uni/controllers/students_controller.py
+++ adm_uni/controllers/students_controller.py
@@ -76,5 +76,5 @@ class StudentController(http.Controller):
     @http.route("/admission/adm_insertId", auth="public", methods=["POST"], cors='*', csrf=False)
     # define una funcion principal 
     def insertId(self, **kw):  
-        data = json.loads(dataRaw)
+        data = json.loads(json.dumps(kw))
         return json.dumps(data[0]['factsId'])
